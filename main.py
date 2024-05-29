e910941b0fb118870eaa3cca073e67d6cb21cd56
Mustafa Mohebi <128157702+MMOHEB1@users.noreply.github.com>
2024-05-29 18:46:17 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# ---------------------------- IMPORTS ------------------------------- #
+from tkinter import *
+
+
+
+# ---------------------------- UNIVERSAL CONSTANCES ------------------------------- #
+BACKGROUND_COLOR = "#B1DDC6"
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+root = Tk()
+root.title("Flash cards")
+root.config(width=500, height=400)
+
+
+
+
+
+root.mainloop()
